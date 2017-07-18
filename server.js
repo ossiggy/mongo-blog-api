bf1edf6fee15973d23bc1f9cd20d57ef4cd86ff2
Ossiggy <mrossig201@gmail.com>
2017-07-18 10:45:32 -0400

    added POST, PUT, DELETE functionality to server.js

--- server.js
+++ server.js
@@ -10,28 +10,89 @@ const app = express()
 app.use(bodyParser.json())
 
 app.get('/posts', (req, res) => {
-    Post
-        .find()
-        .exec()
-        .then(posts => {
-          res.json({
-              posts: posts.map(
-                  (post) => post.apiRepr())
-          })
+  Post
+    .find()
+    .exec()
+    .then(posts => {
+      res.json({
+        posts: posts.map(
+          (post) => post.apiRepr())
       })
-        .catch(
-          err => {
-              console.error(err)
-              res.status(500).json({message: 'Uh Oh! Internal server error'})
-        })
+    })
+    .catch(
+      err => {
+        console.error(err)
+        res.status(500).json({message: 'Uh Oh! Internal server error'})
+    })
 })
 
 app.get('/posts/:id', (req, res)=>{
-    Post
-        .findByID(req.params.id)
-        .then(post => res.json(post.apiRepr()))
-        .catch(err =>{
-            console.error(err)
-            res.status(500).json({message: 'Post not found'})
-        })
+  Post
+    .findByID(req.params.id)
+    .then(post => res.json(post.apiRepr()))
+    .catch(err =>{
+      console.error(err)
+        res.status(500).json({message: 'Post not found'})
+    })
+})
+
+app.post('/posts', (req, res) =>{
+
+  const requiredFields = ['title', 'content', 'author']
+  for(let i=0; i<requiredFields.length; i++){
+    const field = requiredFields[i]
+    if(!(field in req.body)){
+      const message = `Missing \`${field}\` in request body`
+      console.error(message)
+      return res.status(400).send(message)
+    }
+  }
+
+  Post
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author})
+    .then(
+      post => res.status(201).json(post.apiRepr()))
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({message: 'Internal server error'})
+    })
+})
+
+app.put('/posts/:id', (req, res) =>{
+  if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request patch id (${req.params.id} and request body id (${req.body.id}) must match`)
+      console.error(message)
+      res.status(400).json({message: message})
+  }
+
+  const toUpdate = {}
+  const updateableFields = ['title', 'content', 'author']
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field]
+    }
+  })
+
+  Post
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .exec()
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}))
+})
+
+app.delete('/restaurants/:id', (req, res) =>{
+  Post
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(post => res.status(204).end())
+    .catch(err =>res.status(500).json({message: 'Internal server error'}))
+})
+
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not found'})
 })
