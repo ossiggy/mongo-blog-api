7ebbda5c08bb55302cf63668dcc5d2fc07515094
Ossiggy <mrossig201@gmail.com>
2017-07-18 10:53:03 -0400

    added runserver and closeserver to server.js

--- server.js
+++ server.js
@@ -96,3 +96,44 @@ app.delete('/restaurants/:id', (req, res) =>{
 app.use('*', function(req, res) {
   res.status(404).json({message: 'Not found'})
 })
+
+let server
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+
+  return new PRomise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err =>{
+      if (err) {
+        return reject(err)
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`)
+        resolve()
+      })
+      .on('error', err => {
+        mongoose.disconnect()
+        reject(err)
+      })
+    })
+  })
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server')
+      server.close(err => {
+        if(err) {
+          return reject(err)
+        }
+        resolve()
+      })
+    })
+  })
+}
+
+if(require.main === module){
+  runServer().catch(err => console.error(err))
+}
+
+module.exports = {app, runServer, closeServer}
