56ffe0c23acf2c9c1600ae59ea70caa1c3440516
Ossiggy <mrossig201@gmail.com>
2017-07-19 21:55:27 -0400

    changed 500 status message

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/posts', (req, res) => {
     .catch(
       err => {
         console.error(err)
-        res.status(500).json({message: 'Uh Oh! Internal server error'})
+        res.status(500).json({message: 'Internal server error'})
     })
 })
 
